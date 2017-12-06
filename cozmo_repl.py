a8a90581bea0320ae5f8ac549570679dd024c5fb
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-12-06 01:51:40 +0100

    init: add banner and ack to avoid log of connection

--- cozmo_repl.py
+++ cozmo_repl.py
@@ -1,8 +1,18 @@
 import sys
+import os
 from IPython.terminal.embed import InteractiveShellEmbed
 from cozmo_prompt import CozmoPrompt
 import cozmo
 import style as c
+
+banner = """\
+   ______                               ____             __
+  / ____/___  ____  ____ ___  ____     / __ \___  ____  / /
+ / /   / __ \/_  / / __ `__ \/ __ \   / /_/ / _ \/ __ \/ /
+/ /___/ /_/ / / /_/ / / / / / /_/ /  / _, _/  __/ /_/ / /
+\____/\____/ /___/_/ /_/ /_/\____/  /_/ |_|\___/ .___/_/
+                                              /_/
+"""
 
 usage = f"""\
 This is an {c.bold.cyan('IPython')} interactive shell for {c.bold.blue('Cozmo')}.
@@ -15,14 +25,19 @@ All IPython commands work as usual. See below for some useful syntax:
   """
 
 # Creating IPython's history database on the main thread
-ipyshell = InteractiveShellEmbed(banner1=c.bold.blue("\nWelcome to the Cozmo Shell"),
-                                 exit_msg=c.bold.red("Goodbye")+"\n")
+ipyshell = InteractiveShellEmbed(banner1=c.bold.blue(f"\n{banner}\nWelcome to the Cozmo Shell"),
+                                 exit_msg=c.bold.red("Goodbye :)\nWe Hope you had a good time with Cozmo")+"\n")
+
+
+if "COZMO_LOG_LEVEL" not in os.environ:
+    os.environ["COZMO_LOG_LEVEL"] = "ERROR"
+
 
 
 def cozmo_repl(robot: cozmo.robot.Robot):
     """Invoke the ipython shell while connected to cozmo"""
     default_log_level = cozmo.logger.level
-    cozmo.logger.setLevel('WARN')
+    cozmo.logger.setLevel("WARN")
     ipyshell.prompts = CozmoPrompt(ipyshell)
     ipyshell(usage)
     cozmo.logger.setLevel(default_log_level)
