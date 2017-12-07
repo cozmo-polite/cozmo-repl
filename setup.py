0d390d0318f1ba8d2f850dca64a64f74758fae19
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-12-07 19:51:06 +0100

    release: 0.1.1 with added missings GL deps

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='cozmo-repl',
-    version='0.1.0',
-    description='IPython REPL for Cozmo',
+    name="cozmo-repl",
+    version="0.1.1",
+    description="IPython REPL for Cozmo",
     license="Apache2",
-    author='Adrien Becchis',
-    author_email='adriean.khisbe@live.fr',
-    packages=['cozmo_repl'],
+    author="Adrien Becchis",
+    author_email="adriean.khisbe@live.fr",
+    packages=["cozmo_repl"],
     scripts=["cozmo-repl"],
-    install_requires=['cozmo', 'IPython', 'style', 'PyOpenGL', 'PyOpenGL-accelerate', 'Pillow']
+    install_requires=["cozmo", "IPython", "style", "PyOpenGL", "PyOpenGL-accelerate", "Pillow"]
 )
