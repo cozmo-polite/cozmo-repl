048ef45e847e5a907d940e0228a4203e114c60fb
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-12-07 22:09:02 +0100

    test: write first test developping also some test utility

--- tests/cozmo_repl_test.py
+++ tests/cozmo_repl_test.py
@@ -1,7 +1,46 @@
 import unittest
+from cozmo_repl.cozmo_repl import CozmoRepl
+
+
+def noop():
+    pass
+
+
+class Expando(object):
+    pass
+
+
+class FakeCozmo:
+    def __init__(self, robot=None, loggerLevel="INFO", loggerSetLevel=noop):
+        self.logger = Expando()
+        self.robot = robot if robot else Expando()
+        self.logger.level = loggerLevel
+        self.logger.setLevel = loggerSetLevel
+
+    def run_program(self, defun, **kwargs):
+        defun(self.robot)
+
+
+class CheckInvocation():
+    def __init__(self, testself, args):
+        self.args = args
+        self.testself = testself
+        self.invocation = 0
+
+    def invoke(self, args):
+        invocation = self.invocation
+        self.invocation += 1
+        self.testself.assertEquals(args, self.args[invocation])
 
 
 class ReplTestCase(unittest.TestCase):
 
-    def test(self):
-        pass
+    def test_can_create_repl_well_configurated(self):
+        def ipyfake(usage):
+            self.assertEqual(usage, "this is an usage")
+            self.assertIsNotNone(ipyfake.prompts)
+
+        ci = CheckInvocation(self, ["WARN", "INFO"])
+        fake_cozmo = FakeCozmo(loggerSetLevel=ci.invoke)
+        repl = CozmoRepl(fake_cozmo, usage="this is an usage", ipyshell=ipyfake)
+        repl.run()
