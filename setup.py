2b2cd1c74b0ecc9468e6da2f268fb2a31290d458
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-12-07 21:11:32 +0100

    release: 0.2.0 with optional viewers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cozmo-repl",
-    version="0.1.1",
+    version="0.2.0",
     description="IPython REPL for Cozmo",
     license="Apache2",
     author="Adrien Becchis",
