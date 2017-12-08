b74e5cc6e5d1b25cdab9b09a41df89eeef997f05
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-12-08 07:57:22 +0100

    test: add test for verbose and refactor fake logger

--- tests/cozmo_repl_test.py
+++ tests/cozmo_repl_test.py
@@ -11,6 +11,24 @@ class ReplTestCase(unittest.TestCase):
             self.assertIsNotNone(ipyfake.prompts)
 
         ci = CheckInvocation(self, ["WARN", "INFO"])
-        fake_cozmo = FakeCozmo(loggerSetLevel=ci.invoke)
+        fake_cozmo = FakeCozmo(
+            logger_set_level=ci.invoke,
+            pre_check=lambda cozmo_self: self.assertTrue(cozmo_self.logger.disabled),
+            post_check=lambda cozmo_self: self.assertFalse(cozmo_self.logger.disabled)
+        )
         repl = CozmoRepl(fake_cozmo, usage="this is an usage", ipyshell=ipyfake)
         repl.run()
+
+    def test_can_create_repl_well_configurated_verbose(self):
+        def ipyfake(usage):
+            self.assertEqual(usage, "this is an usage")
+            self.assertIsNotNone(ipyfake.prompts)
+
+        ci = CheckInvocation(self, ["WARN", "INFO"])
+        fake_cozmo = FakeCozmo(
+            logger_set_level=ci.invoke,
+            pre_check=lambda cozmo_self: self.assertFalse(cozmo_self.logger.disabled),
+            post_check=lambda cozmo_self: self.assertFalse(cozmo_self.logger.disabled)
+        )
+        repl = CozmoRepl(fake_cozmo, usage="this is an usage", ipyshell=ipyfake)
+        repl.run(verbose=True)
--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -2,34 +2,42 @@ import unittest
 from cozmo_repl.cozmo_repl import CozmoRepl
 
 
-def noop():
+def noop(*args, **kwargs):
     pass
 
 
-class Expando(object):
-    pass
+class Expando: pass
+
+
+class FakeLogger:
+    def __init__(self, level="INFO", setLevel=noop):
+        self.disabled = False
+        self.level = level
+        self.setLevel = setLevel
 
 
 class FakeCozmo:
-    def __init__(self, robot=None, loggerLevel="INFO", loggerSetLevel=noop):
-        self.logger = Expando()
-        self.logger_protocol = Expando()
+    def __init__(self, robot=None, logger_level="INFO", logger_set_level=noop, pre_check=noop, post_check=noop):
+        self.logger = FakeLogger(logger_level, logger_set_level)
+        self.logger_protocol = FakeLogger()
         self.robot = robot if robot else Expando()
-        self.logger.level = loggerLevel
-        self.logger.setLevel = loggerSetLevel
+
+        self.pre_check = pre_check
+        self.post_check = post_check
 
     def run_program(self, defun, **kwargs):
+        self.pre_check(self)
         defun(self.robot)
+        self.post_check(self)
 
 
 class CheckInvocation():
-    def __init__(self, testself, args):
+    def __init__(self, test_self, args):
         self.args = args
-        self.testself = testself
+        self.testself = test_self
         self.invocation = 0
 
     def invoke(self, args):
         invocation = self.invocation
         self.invocation += 1
         self.testself.assertEquals(args, self.args[invocation])
-
